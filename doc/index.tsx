e8f46ad5ff19fb0deeeaf2cba41ab92548e0f52f
Ben <benjamin.mahmoudi@gmail.com>
2022-03-06 18:59:08 +0100

    feat: add position attribute

--- doc/index.tsx
+++ doc/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createUseStyles } from "react-jss";
-import { BananaTooltip } from "../src";
+import { BananaTooltip, BananaTooltipProps } from "../src";
 
 // #region CONSTANTS
 const useStyles = createUseStyles({
 	doc__container: { display: "flex", justifyContent: "center" },
 	doc__exampleContainer: { display: "flex", justifyContent: "space-between", margin: "100px 0" },
-	doc__exampleTitle: { backgroundColor: "#180574", border: "1px solid #575757", borderRadius: "4px", color: "#e5e5e5", margin: 0, padding: "30px" },
+	doc__exampleTitle: { backgroundColor: "#180574", border: "1px solid #575757", borderRadius: "4px", color: "#e5e5e5", margin: "0 60px", padding: "30px" },
 	doc__exampleTooltip: { backgroundColor: "#828282", borderRadius: "6px", color: "#FFFFFF", padding: "8px" },
 });
 // #endregion
@@ -34,6 +34,50 @@ const DocApp = () => {
 							<h3 className={classes.doc__exampleTitle}>With style</h3>
 						</div>
 					</BananaTooltip>
+				</div>
+				<div className={classes.doc__exampleContainer}>
+					{(["topStart", "top", "topEnd"] as BananaTooltipProps["position"][]).map((position) => (
+						<BananaTooltip
+							content={<div className={classes.doc__exampleTooltip}>Hello World!</div>}
+							key={position}
+							position={position}
+						>
+							<h3 className={classes.doc__exampleTitle}>{position}</h3>
+						</BananaTooltip>
+					))}
+				</div>
+				<div className={classes.doc__exampleContainer}>
+					{(["bottomStart", "bottom", "bottomEnd"] as BananaTooltipProps["position"][]).map((position) => (
+						<BananaTooltip
+							content={<div className={classes.doc__exampleTooltip}>Hello World!</div>}
+							key={position}
+							position={position}
+						>
+							<h3 className={classes.doc__exampleTitle}>{position}</h3>
+						</BananaTooltip>
+					))}
+				</div>
+				<div className={classes.doc__exampleContainer}>
+					{(["leftStart", "left", "leftEnd"] as BananaTooltipProps["position"][]).map((position) => (
+						<BananaTooltip
+							content={<div className={classes.doc__exampleTooltip}>Hello World!</div>}
+							key={position}
+							position={position}
+						>
+							<h3 className={classes.doc__exampleTitle}>{position}</h3>
+						</BananaTooltip>
+					))}
+				</div>
+				<div className={classes.doc__exampleContainer}>
+					{(["rightStart", "right", "rightEnd"] as BananaTooltipProps["position"][]).map((position) => (
+						<BananaTooltip
+							content={<div className={classes.doc__exampleTooltip}>Hello World!</div>}
+							key={position}
+							position={position}
+						>
+							<h3 className={classes.doc__exampleTitle}>{position}</h3>
+						</BananaTooltip>
+					))}
 				</div>
 			</div>
 		</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef, useState } from "react";
+import React, { CSSProperties, memo, useEffect, useRef, useState } from "react";
 import { createUseStyles } from "react-jss";
 
 // #region TYPES
@@ -11,6 +11,10 @@ export interface BananaTooltipProps {
 	 * An element to display when hovering the tooltip children
 	 */
 	content: JSX.Element;
+	/**
+	 * Defines where the tooltip should be displayed according to the children position
+	 */
+	position?: "bottom" | "bottomEnd" | "bottomStart" | "left" | "leftEnd" | "leftStart" | "right" | "rightEnd" | "rightStart" | "top" | "topEnd" | "topStart";
 }
 // #endregion
 
@@ -25,6 +29,110 @@ const useStyles = createUseStyles({
 	},
 	tooltip__container: { display: "none", position: "absolute", width: "fit-content" },
 });
+
+// So sad I didn't find how to pass "containedPosition" as a useStyles prop :(
+const tooltipPosition = (elementReference: React.RefObject<HTMLElement>, position: BananaTooltipProps["position"]) => {
+	const elementPosition: DOMRect | undefined = elementReference.current?.getBoundingClientRect();
+	const computedStyle = elementReference.current ? window.getComputedStyle(elementReference.current) : undefined;
+
+	if (elementPosition && computedStyle) {
+		switch (position) {
+		case "topStart":
+
+			return {
+				bottom: `calc(${elementPosition.height + 10}px + ${computedStyle.marginBottom})`,
+				left: computedStyle.marginLeft,
+			};
+		case "top":
+
+			return {
+				bottom: `calc(${elementPosition.height + 10}px + ${computedStyle.marginBottom})`,
+				left: `calc(-100% + ${computedStyle.marginLeft})`,
+				margin: "auto",
+				right: `calc(-100% + ${computedStyle.marginRight}`,
+				textAlign: "center" as CSSProperties["textAlign"],
+			};
+		case "topEnd":
+
+			return {
+				bottom: `calc(${elementPosition?.height + 10}px + ${computedStyle.marginBottom})`,
+				right: computedStyle.marginRight,
+				textAlign: "right" as CSSProperties["textAlign"],
+			};
+		case "rightStart":
+
+			return {
+				left: `calc(${computedStyle.marginLeft} + ${elementPosition.width + 10}px)`,
+				top: computedStyle.marginTop,
+			};
+		case "right":
+
+			return {
+				left: `calc(${computedStyle.marginLeft} + ${elementPosition.width + 10}px)`,
+				top: "50%",
+				transform: "translate(0, -50%)",
+			};
+		case "rightEnd":
+
+			return {
+				bottom: computedStyle.marginBottom,
+				left: `calc(${computedStyle.marginLeft} + ${elementPosition.width + 10}px)`,
+			};
+		case "bottomEnd":
+
+			return {
+				right: computedStyle.marginRight,
+				top: `calc(${computedStyle.marginTop} + ${elementPosition.height + 10}px)`,
+			};
+		case "bottom":
+
+			return {
+				left: `calc(-100% + ${computedStyle.marginLeft})`,
+				margin: "auto",
+				right: `calc(-100% + ${computedStyle.marginRight})`,
+				textAlign: "center" as CSSProperties["textAlign"],
+				top: `calc(${computedStyle.marginTop} + ${elementPosition.height + 10}px)`,
+			};
+		case "bottomStart":
+
+			return {
+				left: computedStyle.marginLeft,
+				top: `calc(${computedStyle.marginTop} + ${elementPosition.height + 10}px)`,
+			};
+		case "leftEnd":
+
+			return {
+				bottom: computedStyle.marginBottom,
+				right: `calc(${computedStyle.marginRight} + ${elementPosition.width + 10}px)`,
+			};
+		case "left":
+
+			return {
+				right: `calc(${computedStyle.marginRight} + ${elementPosition.width + 10}px)`,
+				top: "50%",
+				transform: "translate(0, -50%)",
+			};
+		case "leftStart":
+
+			return {
+				right: `calc(${computedStyle.marginRight} + ${elementPosition.width + 10}px)`,
+				top: computedStyle.marginTop,
+			};
+		default:
+
+			return {
+				bottom: `calc(${elementPosition.height + 10}px + ${computedStyle.marginBottom})`,
+				left: `calc(-100% + ${computedStyle.marginLeft})`,
+				margin: "auto",
+				right: `calc(-100% + ${computedStyle.marginRight}`,
+				textAlign: "center" as CSSProperties["textAlign"],
+			};
+		}
+	} else {
+
+		return {};
+	}
+};
 // #endregion
 
 // #region COMPONENT
@@ -32,11 +140,11 @@ const useStyles = createUseStyles({
  * A simple React tooltip component
  */
 export const BananaTooltip = memo(
-	({ children, content }: BananaTooltipProps) => {
+	({ children, content, position }: BananaTooltipProps) => {
 		const childElement = React.Children.only(children);
 		const ref = useRef<HTMLElement>(null);
 		const rerender = useState(false)[1];
-		const elementPosition = ref?.current?.getBoundingClientRect();
+		const childPosition = ref?.current?.getBoundingClientRect();
 		const classes = useStyles();
 
 		useEffect(
@@ -51,13 +159,10 @@ export const BananaTooltip = memo(
 		return (
 			<div className={classes.component__container} >
 				{React.cloneElement(childElement, { ref: ref })}
-				{elementPosition && (
+				{childPosition && (
 					<div
 						className={classes.tooltip__container}
-						style={{
-							bottom: `${elementPosition.height + 10}px`,
-							left: 0,
-						}}
+						style={ref ? tooltipPosition(ref, position) : {}}
 					>
 						{content}
 					</div>
